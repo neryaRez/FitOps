029d51c6ea5c113e0c2a1baa9689c8353d7fa99e
neryaRez <neryarez@gmail.com>
2026-06-05 15:38:15 +0300

    chore: stop tracking local terraform backend config

--- infra/terraform/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "fitops-nerya-tfstate-455715798206-us-east-1"
-    key            = "tfstate/fitops/nerya/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "fitops-nerya-terraform-locks"
-    encrypt        = true
-  }
-}
