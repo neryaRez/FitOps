ca8f3a39ba5af1276052628b19fa6aec05e44ad4
neryaRez <neryarez@gmail.com>
2026-06-05 16:40:15 +0300

    nn

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -1,3 +1,7 @@
 module "static_site" {
   source = "../../modules/frontend/static-site"
+
+  name          = local.name_prefix
+  bucket_prefix = "${local.name_prefix}-static-site"
+  tags          = local.common_tags
 }
