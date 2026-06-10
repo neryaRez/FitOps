4c21d5a19abeecd689ef0bec5915cc768c93c2b8
neryaRez <neryarez@gmail.com>
2026-06-10 12:18:53 +0300

    h

--- infra/terraform/modules/frontend/static-site/main.tf
+++ infra/terraform/modules/frontend/static-site/main.tf
@@ -73,15 +73,17 @@ resource "aws_cloudfront_distribution" "site" {
   }
 
   custom_error_response {
-    error_code         = 403
-    response_code      = 200
-    response_page_path = "/index.html"
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
   }
 
   custom_error_response {
-    error_code         = 404
-    response_code      = 200
-    response_page_path = "/index.html"
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
   }
 
   restrictions {
